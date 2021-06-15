e92fdfa933b6786349d1b93bcfedee7e72f8af0f
lukasyano <toliusislukas@yahoo.com>
2021-06-15 20:28:49 +0300

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,12 +14,21 @@ application {
 }
 
 repositories {
-    mavenCentral()
+    mavenLocal()
+    jcenter()
+    maven("https://kotlin.bintray.com/ktor")
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers")
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("io.ktor:ktor-html-builder:$ktor_version")
+    implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.31-kotlin-1.2.41")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
